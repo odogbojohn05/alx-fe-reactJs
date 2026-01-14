f8f1ddc6b5b6101a21bcb1541f9dfc842c0872c4
ODOGBO JOHN ADEKUNLE <ebubeodogbo@gmail.com>
2026-01-14 20:06:34 +0100

    Create MainContent.jsx

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#eef', minHeight: '200px' }}>
+      <p style={{ fontSize: '18px', lineHeight: '1.6' }}>
+        Welcome to my React app! Here are some of my favorite cities.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
