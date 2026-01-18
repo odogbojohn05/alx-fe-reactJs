da837dbbb50a67e0c2f958b9ad06ee68dc490fbe
ODOGBO JOHN ADEKUNLE <ebubeodogbo@gmail.com>
2026-01-18 23:25:54 +0100

    Create Footer.jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#222', color: 'white', textAlign: 'center', padding: '10px' }}>
+      <p>&copy; 2026 My React App</p>
+    </footer>
+  );
+}
+
+export default Footer;
